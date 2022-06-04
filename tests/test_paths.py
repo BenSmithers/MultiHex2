51457cd26e5d1a3d1ca009c95628f373f3094058
Ben Smithers <b.smithers.actual@gmail.com>
2022-06-04 13:15:58 -0500

    trying to get setup with github actions

---
+++ tests/test_paths.py
@@ -0,0 +1,10 @@
+from MultiHex2.core.core import Path
+from MultiHex2.core.coordinates import HexID
+
+from PyQt5.QtCore import QPointF
+
+import unittest
+
+class TestPathInits(unittest.TestCase):
+    def test_build_with_HexIDs(self):
+        self.assertEqual(1==1)
